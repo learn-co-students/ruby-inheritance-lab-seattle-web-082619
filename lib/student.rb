e769448edb61ca22ff1d8a6b4c809bec7dac2638
ianhunterharold <ianhunterharold@gmail.com>
2019-12-19 17:19:04 -0800

    passed all testing labs

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student  < User
+
+  def initialize
+    @knowledge=Array.new
+  end 
+
+
+  def learn(string)
+    @knowledge << string
+  end 
+
+  def knowledge 
+    return @knowledge
+  end 
+
+
 
 end
